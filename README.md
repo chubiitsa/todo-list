21f184da27e49d907d30f8fe8e273f49838fbc6b
Anna <anna.chubiy@gmail.com>
2022-11-22 12:33:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Hexlet tests and linter status:
+[![Actions Status](https://github.com/chubiitsa/frontend-project-lvl4/workflows/hexlet-check/badge.svg)](https://github.com/chubiitsa/frontend-project-lvl4/actions)
+
+
+[Try it](https://frontend-lvl4-chubiitsa.herokuapp.com/)
---
+++ src/init.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Hello from './Hello.jsx';
+
+
+const init = () => {
+  const container = document.getElementById('root');
+  const root = createRoot(container);
+  root.render(<Hello />);
+};
+
+export default init;
